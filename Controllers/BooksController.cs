69fbbf864778a2719c1403cba103f9f974f1d28f
98thanhtu <47029265+98thanhtu@users.noreply.github.com>
2020-07-15 20:42:41 +0700

    Add files via upload

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,251 @@
+﻿using AutoMapper;
+using Library.API.Entities;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors/{authorId}/books")]
+    public class BooksController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+        private IMapper _mapper;
+        private IUrlHelper _urlHelper;
+
+        public BooksController(ILibraryRepository libraryRepository, IMapper mapper, IUrlHelper urlHelper)
+        {
+            _libraryRepository = libraryRepository;
+            _mapper = mapper;
+            _urlHelper = urlHelper;
+        }
+
+        [HttpGet(Name = "GetBooksForAuthor")]
+        public IActionResult GetBooksForAuthor(Guid authorId)
+        {
+            if (!_libraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+            var booksForAuthorFromRepo = _libraryRepository.GetBooksForAuthor(authorId);
+            var booksForAuthor = _mapper.Map<IEnumerable<BookDto>>(booksForAuthorFromRepo);
+            booksForAuthor = booksForAuthor.Select(book =>
+            {
+                book = CreateLinksForBook(book);
+                return book;
+            });
+            var wrapper = new LinkedCollectionResourceWrapperDto<BookDto>(booksForAuthor);
+
+            return Ok(CreateLinksForBooks(wrapper));
+        }
+
+        [HttpGet("{id}", Name ="GetBookForAuthor")]
+        public IActionResult GetBookForAuthor(Guid authorId, Guid id)
+        {
+            if (!_libraryRepository.AuthorExists(authorId))
+                return NotFound();
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+            if (bookForAuthorFromRepo == null)
+                return NotFound();
+
+            return Ok(CreateLinksForBook(_mapper.Map<BookDto>(bookForAuthorFromRepo)));
+        }
+
+        [HttpPost(Name = "CreateBookForAuthor")]
+        public IActionResult CreateBookForAuthor(Guid authorId,
+            [FromBody] BookForCreationDto book)
+        {
+            if (book == null)
+                return BadRequest();
+
+            if (book.Description == book.Title)
+                ModelState.AddModelError(nameof(BookForCreationDto),
+                    "The provided description should be different from the title.");
+
+            if (!ModelState.IsValid)
+                return new UnprocessableEntityObjectResult(ModelState);
+
+            if (!_libraryRepository.AuthorExists(authorId))
+                return NotFound();
+
+            var bookEntity = _mapper.Map<Book>(book);
+
+            _libraryRepository.AddBookForAuthor(authorId, bookEntity);
+
+            if (!_libraryRepository.Save())
+                throw new Exception($"Creating a book for author {authorId} failed on save.");
+
+            var bookToReturn = _mapper.Map<BookDto>(bookEntity);
+
+            return CreatedAtRoute("GetBookForAuthor",
+                new { authorId = authorId, id = bookToReturn.Id },
+                CreateLinksForBook(bookToReturn));
+        }   
+
+        [HttpDelete("{id}", Name = "DeleteBookForAuthor")]
+        public IActionResult DeleteBookForAuthor(Guid authorId, Guid id)
+        {
+            if (!_libraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+            if(bookForAuthorFromRepo == null)
+            {
+                return NotFound();
+            }
+            _libraryRepository.DeleteBook(bookForAuthorFromRepo);
+
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception($"Deleting book {id} failed on save.");
+            }
+            return NoContent();
+        }
+
+        [HttpPut("{id}", Name = "UpdateBookForAuthor")]
+        public IActionResult UpdateBookForAuthor(Guid authorId, Guid id, [FromBody] BookForUpdateDto book)
+        {
+            if(book == null)
+                return BadRequest();
+
+            if (book.Description == book.Title)
+                ModelState.AddModelError(nameof(BookForCreationDto),
+                    "The provided description should be different from the title.");
+
+            if (!ModelState.IsValid)
+                return new UnprocessableEntityObjectResult(ModelState);
+
+            if (!_libraryRepository.AuthorExists(authorId))
+                return NotFound();
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+            if(bookForAuthorFromRepo == null)
+            {
+                var bookToAdd = _mapper.Map<Book>(book);
+                bookToAdd.Id = id;
+
+                _libraryRepository.AddBookForAuthor(authorId, bookToAdd);
+
+                if (!_libraryRepository.Save())
+                    throw new Exception($"Upserting book {id} for author {authorId} failed on save.");
+
+                var bookToReturn = _mapper.Map<BookDto>(bookToAdd);
+                return CreatedAtRoute("GetBookForAuthor",
+                    new { authorId  = authorId, id = bookToReturn.Id},
+                    bookToReturn);
+            }
+
+            _mapper.Map(book, bookForAuthorFromRepo);
+            _libraryRepository.UpdateBookForAuthor(bookForAuthorFromRepo);
+
+            if (!_libraryRepository.Save())
+                throw new Exception($"Updating book {id} for author {authorId} failed on save.");
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}", Name = "PartiallyUpdateBookForAuthor")]
+        public IActionResult PartiallyUpdateBookForAuthor(Guid authorId, Guid id,
+            [FromBody] JsonPatchDocument<BookForUpdateDto> patchDoc)
+        {
+            if(patchDoc == null)
+                return BadRequest();
+
+            if (!_libraryRepository.AuthorExists(authorId))
+                return NotFound();
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+            if(bookForAuthorFromRepo == null)
+            {
+                var bookDto = new BookForUpdateDto();
+                patchDoc.ApplyTo(bookDto, ModelState);
+
+                if(bookDto.Description == bookDto.Title)
+                {
+                    ModelState.AddModelError(nameof(BookForUpdateDto),
+                    "The provided description should be different from the title.");
+                }
+
+                TryValidateModel(bookDto);
+
+                if (!ModelState.IsValid)
+                {
+                    return new UnprocessableEntityObjectResult(ModelState);
+                }
+
+                var bookToAdd = _mapper.Map<Book>(bookDto);
+                bookToAdd.Id = id;
+
+                _libraryRepository.AddBookForAuthor(authorId, bookToAdd);
+
+                if (!_libraryRepository.Save())
+                {
+                    throw new Exception($"Upserting book {id} for author {authorId} failed on save.");
+                }
+
+                var bookToReturn = _mapper.Map<BookDto>(bookForAuthorFromRepo);
+
+                return CreatedAtRoute("GetBookForAuthor",
+                    new { authorId = authorId, id = bookToReturn.Id},
+                    bookToReturn);
+            }
+
+            var bookToPatch = _mapper.Map<BookForUpdateDto>(bookForAuthorFromRepo);
+            patchDoc.ApplyTo(bookToPatch, ModelState);
+
+            if (bookToPatch.Description == bookToPatch.Title)
+                ModelState.AddModelError(nameof(BookForCreationDto),
+                    "The provided description should be different from the title.");
+
+            TryValidateModel(bookToPatch);
+
+            if (!ModelState.IsValid)
+                return new UnprocessableEntityObjectResult(ModelState);
+
+            _mapper.Map(bookToPatch, bookForAuthorFromRepo);
+            _libraryRepository.UpdateBookForAuthor(bookForAuthorFromRepo);
+
+            if (!_libraryRepository.Save())
+                throw new Exception($"Patching book {id} for author {authorId} failed on save.");
+
+            return NoContent();
+        }
+
+        private BookDto CreateLinksForBook(BookDto book)
+        {
+            book.Links.Add(new LinkDto(_urlHelper.Link
+                ("GetBookForAuthor", new { id = book.Id }), "self","GET"));
+
+            book.Links.Add(new LinkDto(_urlHelper.Link
+                ("DeleteBookForAuthor", new { id = book.Id }), "delete_book","DELETE"));
+
+            book.Links.Add(new LinkDto(_urlHelper.Link
+                ("UpdateBookForAuthor", new { id = book.Id }), "update_book","PUT"));
+
+            book.Links.Add(new LinkDto(_urlHelper.Link
+                ("PartiallyUpdateBookForAuthor", new { id = book.Id }), "partially_update_book","PATCH"));
+
+            return book;
+        }
+
+        private LinkedCollectionResourceWrapperDto<BookDto> CreateLinksForBooks(
+            LinkedCollectionResourceWrapperDto<BookDto> booksWrapper)
+        {
+            booksWrapper.Links.Add
+                (new LinkDto(_urlHelper.Link("GetBooksForAuthor", new {}),
+                "self",
+                "GET"));
+
+            return booksWrapper;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Library.API.Services;
+using Library.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Library.Services;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using AspNetCoreRateLimit;
+
+namespace Library.API
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(option => option.EnableEndpointRouting = false).AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            });
+
+            services.Configure<MvcOptions>(config =>
+            {
+                var newtonsoftJsonOutputFormatter =
+                    config.OutputFormatters.OfType<NewtonsoftJsonOutputFormatter>().FirstOrDefault();
+
+                if (newtonsoftJsonOutputFormatter != null)
+                {
+                    newtonsoftJsonOutputFormatter.SupportedMediaTypes.Add
+                    ("application/vnd.marvin.author.full+json");
+                    newtonsoftJsonOutputFormatter.SupportedMediaTypes.Add
+                    ("application/vnd.marvin.authorwithdateofdeath.full+json");
+                }
+            });
+
+            services.AddMvc(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+                setupAction.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                setupAction.InputFormatters.Add(new XmlDataContractSerializerInputFormatter(setupAction));
+
+                var xmlDataContractSerializerInputFormatter =
+                new XmlDataContractSerializerInputFormatter(setupAction);
+                xmlDataContractSerializerInputFormatter.SupportedMediaTypes
+                .Add("application/vnd.marvin.authorwithdateofdeath.full+xml");
+                setupAction.InputFormatters.Add(xmlDataContractSerializerInputFormatter);
+
+                var jsonInputFormatter = setupAction.InputFormatters
+                .OfType<NewtonsoftJsonInputFormatter>().FirstOrDefault();
+                if (jsonInputFormatter != null)
+                {
+                    jsonInputFormatter.SupportedMediaTypes
+                    .Add("application/vnd.marvin.author.full+json");
+                    jsonInputFormatter.SupportedMediaTypes
+                    .Add("application/vnd.marvin.authorwithdateofdeath.full+json");
+                }
+                var jsonOutputFormatter = setupAction.OutputFormatters
+                .OfType<NewtonsoftJsonOutputFormatter>().FirstOrDefault();
+
+                if (jsonOutputFormatter != null)
+                {
+                    jsonOutputFormatter.SupportedMediaTypes.Add("application/vnd.marvin.hateoas+json");
+                }
+            });
+
+            var connectionString = Configuration["connectionStrings:libraryDBConnectionString"];
+            services.AddDbContext<LibraryContext>(o => o.UseSqlServer(connectionString));
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddTransient<IPropertyMappingService, PropertyMappingService>();
+
+            services.AddTransient<ITypeHelperService, TypeHelperService>();
+
+            services.AddControllers(setupAction => { setupAction.ReturnHttpNotAcceptable = true; })
+                .AddXmlDataContractSerializerFormatters();
+
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
+
+            services.AddHttpCacheHeaders((expirationModelOptions) =>
+            {
+                expirationModelOptions.MaxAge = 600;
+                expirationModelOptions.CacheLocation = Marvin.Cache.Headers.CacheLocation.Private;
+            },
+           (validationModelOptions) =>
+           {
+               validationModelOptions.MustRevalidate = true;
+           });
+
+            services.AddScoped < IUrlHelper, UrlHelper > (implementationFactory =>
+            {
+                var actionContext =
+                implementationFactory.GetService<IActionContextAccessor>().ActionContext;
+                return new UrlHelper(actionContext);
+            });
+
+            //services.AddMemoryCache();
+            //services.Configure<IpRateLimitOptions>((options) =>
+            //{
+            //    options.GeneralRules = new System.Collections.Generic.List<RateLimitRule>()
+            //    {
+            //        new RateLimitRule()
+            //        {
+            //            Endpoint = "*",
+            //            Limit = 3,
+            //            Period = "5m"
+            //        }
+            //    };
+            //});
+            //services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            //services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, LibraryContext libraryContext)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            libraryContext.EnsureSeedDataForContext();
+
+            app.UseMvc();
+
+            app.UseHttpCacheHeaders();
+
+            //app.UseIpRateLimiting();
+        }
+    }
+}
